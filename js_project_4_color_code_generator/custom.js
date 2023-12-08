e461d88aa85d0820485f5965730fc7f0e449f313
deepti sharma <124440255+deeptisharma10@users.noreply.github.com>
2023-12-09 00:02:37 +0530

    new js project

--- js_project_4_color_code_generator/custom.js
+++ js_project_4_color_code_generator/custom.js
@@ -3,6 +3,9 @@ const randomNumber = Math.floor(Math.random() * 16777215);
 const randomColorCode = "#" + randomNumber.toString(16);
 document.body.style.backgroundColor = randomColorCode;
 document.getElementById("color_code").innerText = randomColorCode
+//document.getElementById("btn").style.backgroundColor = randomColorCode;
+document.getElementById("btn").style.color = randomColorCode;
+navigator.clipboard.writeText(randomColorCode);
 }
 document.getElementById("btn").addEventListener("click", getColor);
 
